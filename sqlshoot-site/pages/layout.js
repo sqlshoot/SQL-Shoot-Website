363a7c99f54e3ba761b39b84672a0a5871b827c5
sqlshoot <sqlshoot@protonmail.com>
2020-12-31 17:33:55 +0000

    Add sample navbar

--- sqlshoot-site/pages/layout.js
+++ sqlshoot-site/pages/layout.js
@@ -1,20 +1,69 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 
 export default function Layout({children}) {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>SQL Shoot</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css" />
       </Head>
 
-      <main className={styles.main}>
-      {children}
-      </main>
+      <body>
+      <nav class="navbar" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+          <a class="navbar-item" href="https://bulma.io">
+            <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" />
+          </a>
 
-      <footer className={styles.footer}>
-        Footer
+          <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+        </div>
+
+        <div id="navbarBasicExample" class="navbar-menu">
+          <div class="navbar-start">
+            <a class="navbar-item">
+              Home
+            </a>
+
+            <a class="navbar-item">
+              Documentation
+            </a>
+
+            <div class="navbar-item has-dropdown is-hoverable">
+              <a class="navbar-link">
+                More
+              </a>
+
+              <div class="navbar-dropdown">
+                <a class="navbar-item">
+                  About
+                </a>
+                <a class="navbar-item">
+                  Contact
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+        <section class="section">
+          <div class="container">
+          {children}
+          </div>
+        </section>
+      </body>
+
+      <footer class="footer">
+        <div class="content has-text-centered">
+          <p>
+            Footer
+          </p>
+        </div>
       </footer>
     </div>
   )
